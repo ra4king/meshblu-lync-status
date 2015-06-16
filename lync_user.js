3ed0b0cc9e419e4018878c0729db78f10c29686b
Roi Atalla <roi.atalla@citrix.com>
2015-06-15 18:10:10 -0700

    Using async library to gather all values before sending. Also changed all double quotes to single quotes and changed tabs to 2 spaces.

--- lync_user.js
+++ lync_user.js
@@ -1,38 +1,150 @@
+'use strict';
+
 module.exports = function(host, xframe_url, authorization, links) {
-	this.host = host;
-	this.xframe_url = xframe_url;
-	this.authorization = authorization;
-	this.links = links;
+  this.host = host;
+  this.xframe_url = xframe_url;
+  this.authorization = authorization;
+  this.links = links;
 
-	var https = require('https');
+  var https = require('https');
 
-	this.getAvailability = function(callback) {
-		var request_properties = {
-			hostname: this.host,
-			path: this.links._embedded.me._links.presence.href,
-			headers: {
-				"Host": this.host,
-				"Authorization": authorization,
-				"Referer": xframe_url,
-				"Accept": "application/json",
-				"Cache-Control": "no-cache"
-			}
-		};
+  this.getAvailability = function(callback) {
+    var request_properties = {
+      hostname: this.host,
+      path: this.links._embedded.me._links.presence.href,
+      headers: {
+        'Host': this.host,
+        'Authorization': authorization,
+        'Referer': xframe_url,
+        'Accept': 'application/json',
+        'Cache-Control': 'no-cache'
+      }
+    };
 
-		https.request(request_properties, function(res) {
-			if(res.statusCode != 200)
-				return callback("Error from webserver. Status code: " + res.statusCode);
+    https.request(request_properties, function(res) {
+      if(res.statusCode != 200)
+        return callback('Error from webserver. Status code: ' + res.statusCode);
 
-			var all_data = '';
+      var all_data = '';
 
-			res.on('data', function(data) {
-				all_data += data;
-			});
+      res.on('data', function(data) {
+        all_data += data;
+      });
 
-			res.on('end', function() {
-				var info = JSON.parse(all_data);
-				callback(null, info.availability);
-			});
-		}).end();
-	}
+      res.on('end', function() {
+        var info = JSON.parse(all_data);
+        callback(null, info.availability);
+      });
+    }).end();
+  };
+
+  this.setLocation = function(location, callback) {
+    var body = JSON.stringify({ 'location': location });
+
+    console.log(body);
+
+    var request_properties = {
+      hostname: this.host,
+      path: this.links._embedded.me._links.location.href,
+      method: 'POST',
+      headers: {
+        'Host': this.host,
+        'Authorization': authorization,
+        'Referer': xframe_url,
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Content-Length': body.length
+      }
+    };
+
+    https.request(request_properties, function(res) {
+      if(res.statusCode != 204)
+        return callback('Error from webserver. Status code: ' + res.statusCode);
+    }).end(body);
+  };
+
+  this.getLocation = function(callback) {
+    var request_properties = {
+      hostname: this.host,
+      path: this.links._embedded.me._links.location.href,
+      headers: {
+        'Host': this.host,
+        'Authorization': authorization,
+        'Referer': xframe_url,
+        'Accept': 'application/json',
+        'Cache-Control': 'no-cache'
+      }
+    };
+
+    https.request(request_properties, function(res) {
+      if(res.statusCode != 200)
+        return callback('Error from webserver. Status code: ' + res.statusCode);
+
+      var all_data = '';
+
+      res.on('data', function(data) {
+        all_data += data;
+      });
+
+      res.on('end', function() {
+        var info = JSON.parse(all_data);
+        callback(null, info.location);
+      });
+    }).end();
+  };
+
+  this.setNote = function(note, callback) {
+    var body = JSON.stringify({ 'message': note });
+
+    console.log(body);
+
+    var request_properties = {
+      hostname: this.host,
+      path: this.links._embedded.me._links.note.href,
+      method: 'POST',
+      headers: {
+        'Host': this.host,
+        'Authorization': authorization,
+        'Referer': xframe_url,
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+        'Content-Length': body.length
+      }
+    };
+
+    https.request(request_properties, function(res) {
+      if(res.statusCode != 204)
+        return callback('Error from webserver. Status code: ' + res.statusCode);
+    }).end(body);
+  }
+
+  this.getNote = function(callback) {
+    var request_properties = {
+      hostname: this.host,
+      path: this.links._embedded.me._links.note.href,
+      headers: {
+        'Host': this.host,
+        'Authorization': authorization,
+        'Referer': xframe_url,
+        'Accept': 'application/json',
+        'Cache-Control': 'no-cache'
+      }
+    };
+
+    https.request(request_properties, function(res) {
+      if(res.statusCode != 200)
+        return callback('Error from webserver. Status code: ' + res.statusCode);
+
+      var all_data = '';
+
+      res.on('data', function(data) {
+        all_data += data;
+      });
+
+      res.on('end', function() {
+        var info = JSON.parse(all_data);
+        callback(null, info.message);
+      });
+    }).end();
+  };
 }
--- lyncstatusnode.js
+++ lyncstatusnode.js
@@ -1,57 +1,96 @@
-var meshblu = require('meshblu')
+'use strict';
+
+var meshblu = require('meshblu');
+var async = require('async');
 var config = require('./config.json');
+var meshblu_config = require('./meshblu.json');
 var lync_auth = require('./lync_auth.js');
 
 var lync_user = undefined;
 
 var conn = meshblu.createConnection({
-	"uuid": "fea309ac-0926-4e56-8d6a-df12586b0f0c",
-	"token": "ea7f6b025eeb2d102134f91507e8d137f749522a",
+  'uuid': meshblu_config.uuid,
+  'token': meshblu_config.token
 });
 
 conn.on('notReady', function(data) {
-	console.log("UUID FAILED AUTHENTICATION!");
-	console.log(data);
+  console.log('UUID FAILED AUTHENTICATION!');
+  console.log(data);
 });
 
 conn.on('ready', function(data) {
-	console.log("UUID AUTHENTICATED!");
-	console.log(data);
+  console.log('UUID AUTHENTICATED!');
+  console.log(data);
 
-	if(!lync_user)
-		connect_to_lync();
+  if(!lync_user)
+    connect_to_lync();
 });
 
 var count = 0;
 conn.on('message', function(message) {
-	process.stdout.write('.');
+  if(lync_user) {
+    if(message.params && message.params.setLocation) {
+      lync_user.setLocation(message.params.setLocation, function(err) {
+        if(err && lync_user) {
+          lync_user = undefined;
+          return connect_to_lync();
+        }
+      });
+    }
+    else if(message.params && message.params.setNote) {
+      lync_user.setNote(message.params.setNote, function(err) {
+        if(err && lync_user) {
+          lync_user = undefined;
+          return connect_to_lync();
+        }
+      });
+    }
+    else {
+      async.parallel([
+        function(callback) { lync_user.getAvailability(callback); },
+        function(callback) { lync_user.getLocation(callback); },
+        function(callback) { lync_user.getNote(callback); }
+      ],
+      function(err, results) {
+        if(err) {
+          if(lync_user) {
+            lync_user = undefined;
+            connect_to_lync();
+          }
 
-	if((count = (count + 1) % 75) == 0) {
-		process.stdout.write('\n');
-	}
+          conn.message('*', {
+            'error': err
+          });
 
-	if(lync_user) {
-		lync_user.getAvailability(function(err, availability) {
-			if(err)
-				return connect_to_lync();
-			
-			conn.message('*', {
-				"presence": err ? "unknown" : availability
-			});
-		});
-	}
-	else {
-		conn.message('*', {
-			"presence": "unknown"
-		});
-	}
+          return;
+        }
+
+        conn.message('*', {
+          'presence': results[0],
+          'location': results[1],
+          'note': results[2]
+        });
+      });
+
+      process.stdout.write('.');
+
+      if((count = (count + 1) % 75) == 0) {
+        process.stdout.write('\n');
+      }
+    }
+  }
+  else {
+    conn.message('*', {
+      'presence': 'unknown'
+    });
+  }
 });
 
 function connect_to_lync() {
-	lync_auth(config, function(err, lync_user_obj) {
-		if(err)
-			return console.log(err);
-		
-		lync_user = lync_user_obj;
-	});
+  lync_auth(config, function(err, lync_user_obj) {
+    if(err)
+      return console.log(err);
+    
+    lync_user = lync_user_obj;
+  });
 }
